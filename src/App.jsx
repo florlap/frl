2fefcbd7f5f059c704dfecd0f6dc9b1b9da1c6aa
florlap <florencialapadatoff@gmail.com>
2022-12-26 23:52:58 -0300

    changes variables

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './styles/components/app.sass';
+//import './styles/components/index.sass';
+
+function App() {
+  return (
+    <div id='portfolio'>
+
+      <h1> NUEVO PORTFOLIO CON REACT Y SASS ..... sass </h1>
+      <h2> Florencia Lapadatoff</h2>
+    </div>
+ 
+  );
+}
+
+export default App;
