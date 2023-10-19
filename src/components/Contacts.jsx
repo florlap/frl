305811ca34d3a3c4d1cb252b36eba7a5e93cebae
florlap <florencialapadatoff@gmail.com>
2023-10-19 01:15:58 -0300

    se hicieron cambios en la seccion de contactos,agregue ubicacion, logo de wsp(cambiar a png)

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,4 +1,5 @@
 
+import { imageListClasses } from '@mui/material'
 import {AiFillPhone, AiOutlineMail, AiFillEnvironment} from 'react-icons/ai'
 import '../styles/components/contacts.sass'
 
@@ -13,25 +14,27 @@ const Contacts = () => {
         <div>
             <h3>Telefono</h3>
             <p>1155833569</p>
+            <a href="https://api.whatsapp.com/send?phone=1155833569" >
+
+            <img src={require('../img/logowasap.jpg')} alt='' width="40" height="40" />
+            </a>
         </div>
     </div>
     <div className='info-card'>
         <AiOutlineMail id='email-icon'/>
         <div>
             <h3>Mail</h3>
-            <a href='mailto:florencialapadatoff@gmail.com'
-            target="_blank"
-            rel="noopener noreferrer"> 
-            
-            <a href='mailto:florencialapadatoff@gmail.com'>florencialapadatoff@gmail.com</a> </a>
-            
+            <a href="mailto:florencialapadatoff@gmail.com" target="_blank"
+            rel="noopener noreferrer" >Enviar Email</a>
         </div>
     </div>
     <div className='info-card'>
         <AiFillEnvironment id='pin-icon'/>
         <div>
             <h3>Localizacion</h3>
-            <p> Buenos Argentina</p>    
+            <a href='https://maps.app.goo.gl/NbEZNiqCuT8pdoYV6' target="_blank"
+            rel="noopener noreferrer" >Buenos Aires, Argentina</a>   
+                 
         </div>
     </div>
 
